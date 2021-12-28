571b0b11c9d97ada679615710a93962777f5a9b4
prierKT <minkyoungtae14@gmail.com>
2021-12-28 17:00:36 +0900

    Practice - get and calculate webtoon's rating

--- web_scraping/8_bs4_gauss.py
+++ web_scraping/8_bs4_gauss.py
@@ -1,24 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
 
-# 웹툰 가우스전자 평점을 받아와서 총점과 평균 계산하기
 url = "https://comic.naver.com/webtoon/list?titleId=675554"
 res = requests.get(url)
 res.raise_for_status()
 
 soup = BeautifulSoup(res.text, "lxml")
 
+# # 제목, 링크 정보
 # cartoons = soup.find_all("td", attrs={"class":"title"})
-# # title = cartoons[0].a.get_text()
-# # link = cartoons[0].a["href"]
-# # print(title)
-# # print("https://comic.naver.com" + link)
+# title = cartoons[0].a.get_text()
+# link = cartoons[0].a["href"]
+# print(title)
+# print("https://comic.naver.com" + link)
 
+# # 한 페이지의 제목, 링크 정보
 # for cartoon in cartoons:
 #   title = cartoon.a.get_text()
 #   link = "https://comic.naver.com" + cartoon.a["href"]
 #   print(title, link)
 
+# 웹툰 가우스전자 평점을 받아와서 한 페이지의 총점과 평균 계산하기
 scores = soup.find_all("div", attrs={"class":"rating_type"})
 total_rates = 0
 

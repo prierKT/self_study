d08bc87864a5c3b64c137858e192a28462276b8a
prierKT <minkyoungtae14@gmail.com>
2022-03-07 20:42:38 +0900

    algorithm with math basic

--- algorithm/Baekjoon_math_basic_2.py
+++ algorithm/Baekjoon_math_basic_2.py
@@ -112,3 +112,57 @@ for i in range(m, n+1):
     print(i)
     
 # %%
+"""
+베르트랑 공준 (Bertrand's postulate)
+
+문제
+베르트랑 공준은 임의의 자연수 n에 대하여,
+
+n보다 크고, 2n보다 작거나 같은 소수는 적어도 하나 존재한다는 내용을 담고 있다.
+
+이 명제는 조제프 베르트랑이 1845년에 추측했고, 파프누티 체비쇼프가 1850년에 증명했다.
+
+예를 들어, 10보다 크고, 20보다 작거나 같은 소수는 4개가 있다.
+
+(11, 13, 17, 19) 또, 14보다 크고, 28보다 작거나 같은 소수는 3개가 있다. (17,19, 23)
+
+자연수 n이 주어졌을 때, n보다 크고, 2n보다 작거나 같은 소수의 개수를 구하는 프로그램을 작성하시오. 
+
+입력
+입력은 여러 개의 테스트 케이스로 이루어져 있다. 각 케이스는 n을 포함하는 한 줄로 이루어져 있다.
+
+입력의 마지막에는 0이 주어진다.
+
+출력
+각 테스트 케이스에 대해서, n보다 크고, 2n보다 작거나 같은 소수의 개수를 출력한다.
+"""
+def prime(n):
+  if n == 1:
+    return False
+  elif n == 2:
+    return True
+  else:
+    for i in range(2, int(n**0.5)+1):
+      if n % i == 0:
+        return False
+  return True
+
+f_list = list(range(1, 123456*2+1))
+p_list = []
+for i in f_list:
+  if prime(i):
+    p_list.append(i)
+
+while True:
+  n = int(input())
+  if n == 0:
+    break
+  
+  c = 0
+  for i in p_list:
+    if n < i <= 2*n:
+      c += 1
+      
+  print(c)
+  
+# %%

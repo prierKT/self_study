4e02150525c273a4ac3ca8683f0cf69e51bc4b63
prierKT <minkyoungtae14@gmail.com>
2022-03-14 21:13:02 +0900

    Find fourth coordinate

--- algorithm/Baekjoon_math_basic_2.py
+++ algorithm/Baekjoon_math_basic_2.py
@@ -254,3 +254,33 @@ x, y, w, h = map(int, input().split())
 print(min(x, w-x, h-y, y))
 
 # %%
+"""
+문제
+세 점이 주어졌을 때,
+
+축에 평행한 직사각형을 만들기 위해서 필요한 네 번째 점을 찾는 프로그램을 작성하시오.
+
+입력
+세 점의 좌표가 한 줄에 하나씩 주어진다.
+
+좌표는 1보다 크거나 같고, 1000보다 작거나 같은 정수이다.
+
+출력
+직사각형의 네 번째 점의 좌표를 출력한다.
+"""
+xs = []
+ys = []
+for _ in range(3):
+  x, y = map(int, input().split())
+  xs.append(x)
+  ys.append(y)
+  
+for i in range(3):
+  if xs.count(xs[i]) == 1:
+    x = xs[i]
+  if ys.count(ys[i]) == 1:
+    y = ys[i]
+    
+print(x, y)
+
+# %%

9437c01157ac399407f946bb00dd296ea95b5b64
prierKT <minkyoungtae14@gmail.com>
2022-03-22 01:14:33 +0900

    star in a specific pattern

--- algorithm/Baekjoon_recursive.py
+++ algorithm/Baekjoon_recursive.py
@@ -9,6 +9,10 @@
 출력
 첫째 줄에 N!을 출력한다.
 """
+from re import L
+from numpy import source
+
+
 def factorial(n):
   if n == 0 or n == 1:
     return 1
@@ -51,5 +55,59 @@ def fibonacci(n):
 
 n = int(input())
 print(fibonacci(n))
+
+# %%
+"""
+문제
+재귀적인 패턴으로 별을 찍어 보자.
+
+N이 3의 거듭제곱(3, 9, 27, ...)이라고 할 때, 크기 N의 패턴은 N×N 정사각형 모양이다.
+
+크기 3의 패턴은 가운데에 공백이 있고, 가운데를 제외한 모든 칸에 별이 하나씩 있는 패턴이다.
+
+***
+* *
+***
+N이 3보다 클 경우, 크기 N의 패턴은 공백으로 채워진 가운데의 (N/3)×(N/3) 정사각형을 크기 N/3의 패턴으로 둘러싼 형태이다.
+
+예를 들어 크기 27의 패턴은 예제 출력 1과 같다.
+
+입력
+첫째 줄에 N이 주어진다. N은 3의 거듭제곱이다. 즉 어떤 정수 k에 대해 N=3**k이며, 이때 1 ≤ k < 8이다.
+
+출력
+첫째 줄부터 N번째 줄까지 별을 출력한다.
+"""
+def star(n, lst):
+  if n == 1:
+    return
+  
+  c = len(lst)
+  r = c // n
+  s = n // 3
+  xy = []
+  for i in range(1, 3*r, 3):
+    for j in range(s):
+      xy.append(s*i+j)
+      
+  for x in xy:
+    for y in xy:
+      lst[x][y] = " "
+      
+  return star(n//3, lst)
+
+n = int(input())
+lst = []
+for i in range(n):
+  l = []
+  for j in range(n):
+    l.append("*")
+  lst.append(l)
+
+star(n, lst)
+for i in range(n):
+  for j in range(n):
+    print(lst[i][j], end="")
+  print()
 
 # %%
